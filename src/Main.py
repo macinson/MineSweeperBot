6c7105c927946ae722a02b74544a404804af7236
Maciej Kozik <macinson.kozik@gmail.com>
2025-06-01 17:46:37 +0200

    Reading the board works

--- src/Main.py
+++ src/Main.py
@@ -1,5 +1,6 @@
 import os
-import threading
+# import threading
+from functools import partial
 
 import pyautogui
 import time
@@ -11,17 +12,20 @@ game_h = 9
 game_w = 9
 mines = 99
 
-init_x = 990
-init_y = 350
+init_x = 972
+init_y = 336
 side_width = 32
 
 lose_pixel = (1108, 293)
 
 win_pixel = (1117, 278)
 
+number_offset = (19, 24)
+unclicked_offset = (29, 29)
+
 smiley_yellow = (253, 254, 85)
 
-tile_gray = (188, 188, 189)
+unclicked_gray = (122, 122, 123)
 
 
 def click_randomly(board):
@@ -30,6 +34,34 @@ def click_randomly(board):
     rand_x = init_x + side_width * tile[0]
     rand_y = init_y + side_width * tile[1]
     pyautogui.click(rand_x, rand_y)
+    time.sleep(0.05)
+    sc = pyautogui.screenshot(region=(940, 230, 1290 - 940, 650 - 230))
+    np.apply_along_axis(partial(update_tile, sc=sc, board=board), 1, available_tiles)
+
+
+def update_tile(t, sc, board):
+    x = init_x + side_width * t[0] - 940
+    y = init_y + side_width * t[1] - 230
+    if sc.getpixel((int(x) + unclicked_offset[0], int(y) + unclicked_offset[1])) != unclicked_gray:
+        if sc.getpixel((int(x) + number_offset[0], int(y) + number_offset[1])) == (0, 0, 244):
+            board[t[0]][t[1]] = 1
+        elif sc.getpixel((int(x) + number_offset[0], int(y) + number_offset[1])) == (52, 121, 32):
+            board[t[0]][t[1]] = 2
+        elif sc.getpixel((int(x) + number_offset[0], int(y) + number_offset[1])) == (232, 45, 35):
+            board[t[0]][t[1]] = 3
+        elif sc.getpixel((int(x) + number_offset[0], int(y) + number_offset[1])) == (0, 0, 118):
+            board[t[0]][t[1]] = 4
+        elif sc.getpixel((int(x) + number_offset[0], int(y) + number_offset[1])) == (111, 16, 11):
+            board[t[0]][t[1]] = 5
+        elif sc.getpixel((int(x) + number_offset[0], int(y) + number_offset[1])) == (52,121,122):
+            board[t[0]][t[1]] = 6
+        elif sc.getpixel((int(x) + number_offset[0], int(y) + number_offset[1])) == (47,47,47):
+            board[t[0]][t[1]] = 7
+        elif sc.getpixel((int(x) + number_offset[0], int(y) + number_offset[1])) == (122,122,123):
+            board[t[0]][t[1]] = 8
+        else:
+            board[t[0]][t[1]] = 0
+
 
 def exit():
     while True:
@@ -48,6 +80,10 @@ def main():
     # pull up the window
     pyautogui.getWindowsWithTitle("Minesweeper Online")[0].maximize()
 
+    time.sleep(1)
+
+    # reset board
+    pyautogui.click(lose_pixel[0], lose_pixel[1])
     board = np.full((game_w, game_h), -1)
     start_time = time.time()
 
@@ -57,9 +93,12 @@ def main():
         if pyautogui.pixel(lose_pixel[0], lose_pixel[1]) != smiley_yellow:
             pyautogui.click(lose_pixel[0], lose_pixel[1])
             board = np.full((game_w, game_h), -1)
+            time.sleep(0.1)
         click_randomly(board)
+        print(board.T)
         # to see what's happening
         # time.sleep(1)
+
 
 if __name__ == "__main__":
     main()
