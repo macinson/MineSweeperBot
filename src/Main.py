1c166a79a657929cfe86fa5d864b151694fd40ba
Maciej Kozik <macinson.kozik@gmail.com>
2025-06-01 23:12:36 +0200

    Simplest rule implemented, crushes beginner first time

--- src/Main.py
+++ src/Main.py
@@ -10,7 +10,7 @@ import numpy as np
 
 game_h = 9
 game_w = 9
-mines = 99
+mines = 10
 
 init_x = 972
 init_y = 336
@@ -27,6 +27,7 @@ smiley_yellow = (253, 254, 85)
 
 unclicked_gray = (122, 122, 123)
 
+MARKER = 99
 
 def click_randomly(board):
     available_tiles = np.argwhere(board == -1)
@@ -34,9 +35,28 @@ def click_randomly(board):
     rand_x = init_x + side_width * tile[0]
     rand_y = init_y + side_width * tile[1]
     pyautogui.click(rand_x, rand_y)
+
+def educated_click(board):
+    safe_tiles = np.argwhere(board == -2)
+    available_tiles = np.argwhere(np.logical_or(board == -1, board == -2))
+    if safe_tiles.size > 0:
+        for tile in safe_tiles:
+            x = init_x + side_width * tile[0]
+            y = init_y + side_width * tile[1]
+            pyautogui.click(x, y)
+    else:
+        tile = random.choice(available_tiles)
+        x = init_x + side_width * tile[0]
+        y = init_y + side_width * tile[1]
+        pyautogui.click(x, y)
     time.sleep(0.05)
     sc = pyautogui.screenshot(region=(940, 230, 1290 - 940, 650 - 230))
     np.apply_along_axis(partial(update_tile, sc=sc, board=board), 1, available_tiles)
+
+    # place flags on our board
+    place_flags(board)
+    # identify no-bomb spots
+    update_board(board)
 
 
 def update_tile(t, sc, board):
@@ -62,6 +82,34 @@ def update_tile(t, sc, board):
         else:
             board[t[0]][t[1]] = 0
 
+def place_flags(board):
+    tiles = np.argwhere(np.logical_and(board >= 1, board <= 8))
+    for tile in tiles:
+        temp = board[tile[0]][tile[1]]
+        board[tile[0]][tile[1]] = MARKER
+        neighbours = board[tile[0]-1 : tile[0]+2, tile[1]-1 : tile[1]+2]
+        center = np.argwhere(neighbours == MARKER)
+        board[tile[0]][tile[1]] = temp
+        possible_bombs = np.argwhere(np.logical_or(neighbours == -1, neighbours == 9))
+
+        if possible_bombs.shape[0] == board[tile[0]][tile[1]]:
+            for t in possible_bombs:
+                board[tile[0] + t[0] - center[0][0]][tile[1] + t[1]- center[0][1]] = 9
+
+def update_board(board):
+    tiles = np.argwhere(np.logical_and(board >= 1, board <= 8))
+    for tile in tiles:
+        temp = board[tile[0]][tile[1]]
+        board[tile[0]][tile[1]] = MARKER
+        neighbours = board[tile[0] - 1: tile[0] + 2, tile[1] - 1: tile[1] + 2]
+        center = np.argwhere(neighbours == MARKER)
+        board[tile[0]][tile[1]] = temp
+        bombs = np.argwhere(neighbours == 9)
+        unknown_tiles = np.argwhere(neighbours == -1)
+
+        if bombs.shape[0] == board[tile[0]][tile[1]]:
+            for t in unknown_tiles:
+                board[tile[0] + t[0] - center[0][0]][tile[1] + t[1]- center[0][1]] = -2
 
 def exit():
     while True:
@@ -94,8 +142,7 @@ def main():
             pyautogui.click(lose_pixel[0], lose_pixel[1])
             board = np.full((game_w, game_h), -1)
             time.sleep(0.1)
-        click_randomly(board)
-        print(board.T)
+        educated_click(board)
         # to see what's happening
         # time.sleep(1)
 
