c0e5a551ab18f43311475354dac7605b24fa861d
Maciej Kozik <macinson.kozik@gmail.com>
2025-06-02 18:53:13 +0200

    Generalized it to expert as well. It's too dumb tho to solve it, need to implement more advanced rules

--- src/Main.py
+++ src/Main.py
@@ -8,17 +8,12 @@ import random
 import keyboard
 import numpy as np
 
-game_h = 9
-game_w = 9
-mines = 10
-
-init_x = 972
-init_y = 336
 side_width = 32
+MARKER = 99
 
-lose_pixel = (1108, 293)
+lose_pixel = (18, 35)
 
-win_pixel = (1117, 278)
+win_pixel = (27, 20)
 
 number_offset = (19, 24)
 unclicked_offset = (29, 29)
@@ -27,13 +22,28 @@ smiley_yellow = (253, 254, 85)
 
 unclicked_gray = (122, 122, 123)
 
-MARKER = 99
+### board-specific settings
+
+game_h = 16
+game_w = 30
+mines = 99
+
+# init = (972,336)
+init = (952,336)
+
+# game_button = (1090,258)
+# game_button = (1202,258)
+game_button = (1406,258)
+
+# game_size = (1290 - 940, 650 - 230)
+# game_size = (1940 - 940, 870 - 230)
+game_size = (1912 - 940, 847 - 230)
 
 def click_randomly(board):
     available_tiles = np.argwhere(board == -1)
     tile = random.choice(available_tiles)
-    rand_x = init_x + side_width * tile[0]
-    rand_y = init_y + side_width * tile[1]
+    rand_x = init[0] + side_width * tile[0]
+    rand_y = init[1] + side_width * tile[1]
     pyautogui.click(rand_x, rand_y)
 
 def educated_click(board):
@@ -41,16 +51,16 @@ def educated_click(board):
     available_tiles = np.argwhere(np.logical_or(board == -1, board == -2))
     if safe_tiles.size > 0:
         for tile in safe_tiles:
-            x = init_x + side_width * tile[0]
-            y = init_y + side_width * tile[1]
+            x = init[0] + side_width * tile[0]
+            y = init[1] + side_width * tile[1]
             pyautogui.click(x, y)
     else:
         tile = random.choice(available_tiles)
-        x = init_x + side_width * tile[0]
-        y = init_y + side_width * tile[1]
+        x = init[0] + side_width * tile[0]
+        y = init[1] + side_width * tile[1]
         pyautogui.click(x, y)
     time.sleep(0.05)
-    sc = pyautogui.screenshot(region=(940, 230, 1290 - 940, 650 - 230))
+    sc = pyautogui.screenshot(region=(init[0], init[1], game_size[0], game_size[1]))
     np.apply_along_axis(partial(update_tile, sc=sc, board=board), 1, available_tiles)
 
     # place flags on our board
@@ -60,8 +70,8 @@ def educated_click(board):
 
 
 def update_tile(t, sc, board):
-    x = init_x + side_width * t[0] - 940
-    y = init_y + side_width * t[1] - 230
+    x = side_width * t[0]
+    y = side_width * t[1]
     if sc.getpixel((int(x) + unclicked_offset[0], int(y) + unclicked_offset[1])) != unclicked_gray:
         if sc.getpixel((int(x) + number_offset[0], int(y) + number_offset[1])) == (0, 0, 244):
             board[t[0]][t[1]] = 1
@@ -131,15 +141,15 @@ def main():
     time.sleep(1)
 
     # reset board
-    pyautogui.click(lose_pixel[0], lose_pixel[1])
+    pyautogui.click(game_button[0], game_button[1])
     board = np.full((game_w, game_h), -1)
     start_time = time.time()
 
     while not keyboard.is_pressed('q') and time.time() < start_time + 600:
-        if pyautogui.pixel(win_pixel[0], win_pixel[1]) != smiley_yellow:
+        if pyautogui.pixel(game_button[0] + win_pixel[0], game_button[1] + win_pixel[1]) != smiley_yellow:
             break
-        if pyautogui.pixel(lose_pixel[0], lose_pixel[1]) != smiley_yellow:
-            pyautogui.click(lose_pixel[0], lose_pixel[1])
+        if pyautogui.pixel(game_button[0] + lose_pixel[0], game_button[1] + lose_pixel[1]) != smiley_yellow:
+            pyautogui.click(game_button[0], game_button[1])
             board = np.full((game_w, game_h), -1)
             time.sleep(0.1)
         educated_click(board)
